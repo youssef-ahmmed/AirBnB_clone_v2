8a4152ec5beb7acb50d8c2a97d87128ce0de9d7f
Salma Ahmed <107444767+Sallmahussien@users.noreply.github.com>
2023-09-19 01:24:06 +0300

    Update basemodel, state and city

--- models/base_model.py
+++ models/base_model.py
@@ -3,18 +3,26 @@
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column('id', String(60), primary_key=True, nullable=False)
+    created_at = Column('created_at', DateTime, nullable=False,
+                        default=datetime.utcnow())
+    updated_at = Column('updated_at', DateTime, nullable=False,
+                        default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,6 +39,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -41,4 +50,9 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary.pop("_sa_instance_state", None)
         return dictionary
+
+    def delete(self):
+        """Delete the current instance from the storage"""
+        models.storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column('name', String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete')
+
+    @property
+    def cities(self):
+        return [city for city in self.cities if city.state_id == self.id]
