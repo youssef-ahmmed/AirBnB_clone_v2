eada1dc17d93b953dc9c6e624e12b33b4c7cd6e2
salma hussien <salmaa.hussien1@gmail.com>
2023-09-19 08:02:26 +0300

    Update amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,58 +1,15 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
-import uuid
-from datetime import datetime
+""" State Module for HBNB project """
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-
-import models
-
-Base = declarative_base()
+from models.base_model import BaseModel, Base
 
 
-class BaseModel:
-    """A base class for all hbnb models"""
-    id = Column('id', String(60), primary_key=True, nullable=False)
-    created_at = Column('created_at', DateTime, nullable=False,
-                        default=datetime.utcnow())
-    updated_at = Column('updated_at', DateTime, nullable=False,
-                        default=datetime.utcnow())
+class Amenity(BaseModel, Base):
+    """This class defines an amenity by various attributes"""
+    __tablename__ = 'amenities'
 
-    def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
-
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
-
-    def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
-
-    def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary.pop("_sa_instance_state", None)
-        return dictionary
-
-    def delete(self):
-        """Delete the current instance from the storage"""
-        models.storage.delete(self)
+    name = Column('name', String(128), nullable=False)
+    place_amenities = relationship('Place', secondary='place_amenity',
+                                   viewonly=False)
