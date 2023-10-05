1a9ace7b248255aab37f3ce5c483292d3d36f935
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-06 01:54:06 +0300

    Creates and distributes an archive to your web servers

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Full deployment"""
+
+import os
+
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+
+def deploy():
+    """Full deployment"""
+    try:
+        files = os.listdir("versions")
+        if len(files) == 1:
+            archive_path = "versions/" + files[0]
+
+    except Exception:
+        archive_path = do_pack()
+
+    return do_deploy(archive_path)
