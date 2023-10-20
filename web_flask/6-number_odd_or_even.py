2591eb2b099f2a6c7533b015289bc961b31a2f5e
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-20 17:01:43 +0300

    Add mandatory task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """display hello holberton"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """display holberton"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    """display C concatenating with any text"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """display Python concatenating with any text"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """display n is a number with a condition"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """display n is a template number with a condition"""
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    """display n is even or odd number"""
+    n_type = 'even' if (n % 2 == 0) else 'odd'
+    return render_template('6-number_odd_or_even.html', n_type=n_type)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
