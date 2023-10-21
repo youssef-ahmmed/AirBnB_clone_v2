be4cf6b342ce858d2493938d766781e85d1a6fa7
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-21 17:25:56 +0300

    remove stupid line phase 2

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -7,23 +7,22 @@ from models import storage
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-data = storage.all("State")
-
 
 @app.route('/states')
 def states():
     """Get all state data"""
+    data = storage.all("State")
     return render_template("9-states.html",
                            states=data)
 
 
 @app.route('/states/<id>')
 def states_by_id(id):
-    for state in data.values():
+    """State by id"""
+    for state in storage.all("State").values():
         if state.id == id:
-            obj = state
             return render_template("9-states.html",
-                                   id=id, state=obj, notfound=False)
+                                   id=id, state=state, notfound=False)
     return render_template("9-states.html", notfound=True)
 
 
