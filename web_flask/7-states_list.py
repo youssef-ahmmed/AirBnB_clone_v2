81c8ea3add580f3a3fc1df8c91368d44117ac66f
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-21 11:47:51 +0300

    Solve database bug

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,25 +3,21 @@
 from flask import Flask, render_template
 
 from models import storage
-from models.state import State
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
-data = storage.all(State)
 
 
 @app.route('/states_list')
 def states():
-    """"""
-    name_id_pairs = [(value["name"], value["id"]) for value in data.values()]
-    name_id_pairs.sort(key=lambda name: name[0])
-    return render_template('7-states_list.html',
-                           name_id_pairs=name_id_pairs)
+    """Get all state data"""
+    data = storage.all("State")
+    return render_template("7-states_list.html",
+                           states=data)
 
 
 @app.teardown_appcontext
-def terminate():
+def terminate(exc):
+    """Close SQLAlchemy session"""
     storage.close()
 
 
